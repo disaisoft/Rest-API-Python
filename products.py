02a860966b95da7b931bd298447ccf3ad4e9f001
disaisoft <disaisoft@gmail.com>
2020-01-28 10:51:44 -0500

    iniciando app

---
+++ products.py
@@ -0,0 +1,6 @@
+products = [
+    {"name": "laptop", "price": 800, "quantity": 4},
+    {"name": "mouse", "price": 50, "quantity": 10},
+    {"name": "monitor", "price": 200 "quantity": 3},
+    {"name": "cpu", "price": 950 "quantity": 5},
+]
